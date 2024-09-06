6038e85d7373321b7ef36946613be0212dce1dca
jsunnykhan <jsunnykhan2@gmail.com>
2024-09-06 15:16:03 +0600

    logout button add

--- src/app/(authenticate)/page.tsx
+++ src/app/(authenticate)/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { logout } from '@/lib/actions/authenticate';
 import { useGetHealthReportQuery } from '@/services/health-service';
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
   if (isLoading) return <div className="px-5 py-7"> loading ...</div>;
 
   return (
-    <main className="flex min-h-screen items-start px-5 py-7">
+    <main className="flex min-h-screen items-start px-5 py-7 justify-between">
       <div className="flex space-x-2 items-center">
         <span className="relative flex h-3 w-3">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
@@ -16,6 +17,8 @@ export default function Home() {
         </span>
         <p>{data.message}</p>
       </div>
+
+      <button className='px-3 py-2 bg-purple-600 text-white rounded ' onClick={async ()=>logout() }>Logout</button>
     </main>
   );
 }
--- src/lib/actions/authenticate.ts
+++ src/lib/actions/authenticate.ts
@@ -1,6 +1,11 @@
-'use server';
-import { signIn } from '@/lib/next-auth/auth';
+"use server";
+import { signIn, signOut } from "@/lib/next-auth/auth";
 
 export async function credentialsSigning(formData: FormData) {
-  return await signIn('credentials', formData);
+  formData.append("redirect", "false");
+  return await signIn("credentials", formData);
+}
+
+export async function logout() {
+  return await signOut();
 }
